88e264e4c81aa5a5f93d82cacf2081363671d2b7
quandn36 <quandn36@gmail.com>
2021-08-04 14:18:17 +0700

    bai 2: template engine, using pug as template engine

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
+// import libraries
 const express = require('express');
 const app     = express();
 const port    = 3000;
+const pug     = require('pug');
 
+// setting
+app.set('view engine', 'pug'); // set pug is view engine using for project
+app.set('views', './views'); // set views is view using for project
+
+// route and controller
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.render('index', {name: 'quan'});
 });
 
+// port listening
 app.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`);
 });
