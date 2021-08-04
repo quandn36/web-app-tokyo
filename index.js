13e001e5dd4882c90fbba4b0d0298db4b1408e9d
quandn36 <quandn36@gmail.com>
2021-08-05 01:00:43 +0700

    using post request create new user

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ const pug     = require('pug');
 app.set('view engine', 'pug'); // set pug is view engine using for project
 app.set('views', './views'); // set views is view using for project
 
+
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
 const users = [
     { name: 'Dong', age: 18 },
     { name: 'Tay', age: 19 },
@@ -35,6 +39,16 @@ app.get('/users/search', (req, res, next) => {
     }else{
         res.render('users/index', { users: users, search: search });
     }
+});
+
+app.get('/users/create', (req, res, next) => {
+    res.render('users/create');
+});
+
+app.post('/users/create', (req, res, next) => {
+    users.push(req.body);
+    res.redirect('/users');
+
 });
 
 // port listening
