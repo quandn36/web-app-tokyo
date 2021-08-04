483df57553a1b84008bea078c291135ac92bb20a
quandn36 <quandn36@gmail.com>
2021-08-05 00:21:29 +0700

    bai3: query parameter

--- index.js
+++ index.js
@@ -8,9 +8,33 @@ const pug     = require('pug');
 app.set('view engine', 'pug'); // set pug is view engine using for project
 app.set('views', './views'); // set views is view using for project
 
+const users = [
+    { name: 'Dong', age: 18 },
+    { name: 'Tay', age: 19 },
+    { name: 'Nam', age: 20 },
+    { name: 'Bac', age: 21 },
+];
+
+
 // route and controller
-app.get('/', (req, res) => {
+app.get('/', (req, res, next) => {
     res.render('index', {name: 'quan'});
+});
+
+app.get('/users', (req, res, next) => {
+    res.render('users/index', { users: users });
+});
+
+app.get('/users/search', (req, res, next) => {
+    let search = req.query.q;
+    let findUser = users.filter((user) => {
+        return user.name.toLowerCase().indexOf(search) !== -1;
+    });
+    if(findUser !== null){
+        res.render('users/index', { users: findUser, search: search });
+    }else{
+        res.render('users/index', { users: users, search: search });
+    }
 });
 
 // port listening
