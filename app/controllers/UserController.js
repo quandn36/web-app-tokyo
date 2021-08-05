fc776973220520ae68cbee884514e63ed49177ec
quandn36 <quandn36@gmail.com>
2021-08-05 22:19:14 +0700

    master layout pug templates

---
+++ app/controllers/UserController.js
@@ -0,0 +1,45 @@
+// require database
+const db      = require('../../database');
+const shortId = require('shortid');
+
+users = db.get('users'); // khởi tạo object users để thao tác
+
+class UserController {
+    index(req, res, next) {
+        res.render('users/index', { users: users.value() });
+    }
+
+    search(req, res, next) {
+        let search = req.query.q;
+        let findUser = users.value().filter((user) => {
+            return user.name.toLowerCase().indexOf(search) !== -1;
+        });
+    
+        if(findUser !== null){
+            res.render('users/index', { users: findUser });
+        }else{
+            res.render('users/index', { users: users });
+        }
+    }
+
+    create(req, res, next) {
+        res.render('users/create');
+    }
+
+    store(req, res, next) {
+        req.body.id = shortId.generate(); // auto generate id
+        users.push(req.body).write();
+        res.redirect('/users');
+    
+    }
+
+    show(req, res, next) {
+        let id = req.params.id;
+        const user = users.find({id: id}).value(); //tìm user trong file db
+        res.render('users/show', { user: user });
+    
+    }
+}
+
+
+module.exports = new UserController;
--- routers/users.router.js
+++ routers/users.router.js
@@ -1,41 +1,14 @@
-const express = require('express');
-const route   = express.Router();
-const shortId = require('shortid');
-const db      = require('../database');
+const express        = require('express');
+const route          = express.Router();
+const userController = require('../app/controllers/UserController');
 
-users = db.get('users');
 
-route.get('/', (req, res, next) => {
-    res.render('users/index', { users: users.value() });
-});
+route.get('/create', userController.create); // user create page
+route.post('/create', userController.store); // save new user
+route.get('/search', userController.search); // user search page
+route.get('/:id', userController.show); // user show page
+route.get('/', userController.index);// homepage
 
-route.get('/search', (req, res, next) => {
-    let findUser = users.value().filter((user) => {
-        return user.name.toLowerCase().indexOf(search) !== -1;
-    });
 
-    if(findUser !== null){
-        res.render('users/index', { users: findUser });
-    }else{
-        res.render('users/index', { users: users });
-    }
-});
-
-route.get('/create', (req, res, next) => {
-    res.render('users/create');
-});
-
-route.get('/:id', (req, res, next) => {
-    let id = req.params.id;
-    const user = users.find({id: id}).value(); //tìm user trong file db
-    res.render('users/show', { user: user });
-
-});
-route.post('/create', (req, res, next) => {
-    req.body.id = shortId.generate(); // auto generate id
-    users.push(req.body).write();
-    res.redirect('/users');
-
-});
 
 module.exports = route;
